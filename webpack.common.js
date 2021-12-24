c1427ab0833513117705f80d7cfb49c4480e3052
sabiq7392 <sabiqmuhammad@gmail.com>
2021-12-24 15:58:39 +0700

    deploy

--- webpack.common.js
+++ webpack.common.js
@@ -5,10 +5,33 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimize: true,
+  },
   module: {
     rules: [
       {
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,30 +11,6 @@ module.exports = merge(common, {
     compress: true,
     port: 1300,
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 70000,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      automaticNameDelimiter: '~',
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    minimize: true,
-  },
   plugins: [
     new BundleAnalyzerPlugin(),
     new CleanWebpackPlugin(),
